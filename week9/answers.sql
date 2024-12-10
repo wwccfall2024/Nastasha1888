a93013b675af214b82840eb60ac29224e9d0d1f9
Nastasha Bennett <129317822+Nastasha1888@users.noreply.github.com>
2024-12-10 11:23:58 -0800

    Added cursors

--- week9/answers.sql
+++ week9/answers.sql
@@ -4,17 +4,17 @@ USE social;
 
 CREATE TABLE users (
   user_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  email VARCHAR(100),
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE sessions (
   session_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id INT UNSIGNED,
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   CONSTRAINT users_fk_sessions
     FOREIGN KEY (user_id) REFERENCES users(user_id)
   ON DELETE CASCADE
@@ -35,9 +35,9 @@ CREATE TABLE friends (
 CREATE TABLE posts (
   post_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id INT UNSIGNED,
-  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  content VARCHAR(255),
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  content VARCHAR(255) NOT NULL,
   CONSTRAINT posts_fk_users
     FOREIGN KEY (user_id) REFERENCES users(user_id)
   ON DELETE CASCADE
@@ -55,7 +55,48 @@ CREATE TABLE notifications (
   ON DELETE CASCADE
 );
 
-CREATE  OR REPLACE VIEW notification_posts AS
+DELIMITER ;;
+
+CREATE PROCEDURE fetch_user_ids()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE current_post_id INT;
+    DECLARE current_user_id INT;
+
+    DECLARE post_id_cursor CURSOR FOR 
+      SELECT post_id 
+        FROM notifications;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN post_id_cursor;
+    post_loop: LOOP
+        FETCH post_id_cursor INTO current_post_id;
+        IF done THEN
+            LEAVE post_loop;
+        END IF;
+
+        SELECT user_id INTO current_user_id
+        FROM posts
+        WHERE post_id = current_post_id;
+
+        INSERT INTO fetched_users 
+          (current_post_id, current_user_id) 
+        VALUES 
+          (current_post_id, current_user_id);
+    END LOOP;
+
+    CLOSE post_id_cursor;
+END ;;
+
+DELIMITER ;
+
+CREATE TABLE fetched_users (
+    post_id INT,
+    user_id INT
+);
+
+CREATE OR REPLACE VIEW notification_posts AS
 SELECT 
     n.user_id,
     u.first_name,
@@ -63,44 +104,87 @@ SELECT
     p.post_id,
     p.content
 FROM notifications n
-JOIN users u ON u.user_id = (
-    SELECT user_id 
-    FROM posts 
-    WHERE post_id = n.post_id
-)
-JOIN posts p ON p.post_id = n.post_id
+INNER JOIN fetched_users fu 
+  ON fu.post_id = n.post_id
+INNER JOIN users u 
+  ON u.user_id = fu.user_id
+INNER JOIN posts p 
+  ON p.post_id = n.post_id
 ORDER BY p.post_id;
 
-DELIMITER ;;
+CALL fetch_user_ids();
 
 CREATE TRIGGER after_user_insert 
 AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-    INSERT INTO posts (user_id, content)
-    VALUES (NEW.user_id, CONCAT(NEW.first_name, ' ', NEW.last_name, ' just joined!'));
-    
-    INSERT INTO notifications (user_id, post_id)
-    SELECT u.user_id, LAST_INSERT_ID()
-    FROM users u
-    WHERE u.user_id != NEW.user_id;
+INSERT INTO posts 
+  (user_id, content)
+VALUES 
+  (NEW.user_id, CONCAT(NEW.first_name, ' ', NEW.last_name, ' just joined!'));
+
+DECLARE done INT DEFAULT FALSE;
+DECLARE current_user_id INT;
+
+DECLARE users_cursor CURSOR FOR 
+  SELECT user_id 
+  FROM users 
+  WHERE user_id != NEW.user_id;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+OPEN users_cursor;
+
+users_loop: LOOP
+    FETCH users_cursor INTO current_user_id;
+    IF done THEN
+        LEAVE users_loop;
+    END IF;
+
+    INSERT INTO notifications 
+      (user_id, post_id)
+    VALUES 
+      (current_user_id, LAST_INSERT_ID());
+END LOOP;
+
+CLOSE users_cursor;
 END;;
 
 CREATE PROCEDURE add_post(IN user_id INT UNSIGNED, IN content VARCHAR(255))
 BEGIN
+  
     INSERT INTO posts (user_id, content)
     VALUES (user_id, content);
-    
-    INSERT INTO notifications (user_id, post_id)
-    SELECT friend_id, LAST_INSERT_ID()
-    FROM friends f
-    WHERE f.user_id = user_id;
+
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE friend_id INT;
+
+    DECLARE friends_cursor CURSOR FOR 
+      SELECT friend_id 
+      FROM friends 
+      WHERE user_id = user_id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN friends_cursor;
+
+    friends_loop: LOOP
+        FETCH friends_cursor INTO friend_id;
+        IF done THEN
+            LEAVE friends_loop;
+        END IF;
+
+        INSERT INTO notifications 
+          (user_id, post_id)
+        VALUES 
+          (friend_id, LAST_INSERT_ID());
+    END LOOP;
+
+    CLOSE friends_cursor;
 END;;
+
 CREATE EVENT clean_old_sessions
 ON SCHEDULE EVERY 10 SECOND
 DO
     DELETE FROM sessions 
-    WHERE updated_on < DATE_SUB(NOW(), INTERVAL 2 HOUR)
-    AND created_on < DATE_SUB(NOW(), INTERVAL 2 HOUR);;
+    WHERE updated_on < DATE_SUB(NOW(), INTERVAL 2 HOUR);;
 
 DELIMITER ;

a2e5dc1e6b2212fe6e578c0a88beadb99971a234
Nastasha Bennett <129317822+Nastasha1888@users.noreply.github.com>
2024-12-11 14:45:34 -0800

    Update answers.sql

--- week9/answers.sql
+++ week9/answers.sql
@@ -55,44 +55,7 @@ CREATE TABLE notifications (
   ON DELETE CASCADE
 );
 
-CREATE TABLE fetched_users (
-    post_id INT,
-    user_id INT
-);
-
 DELIMITER ;;
-
-CREATE PROCEDURE fetch_user_ids()
-BEGIN
-  DECLARE done INT DEFAULT FALSE;
-  DECLARE current_post_id INT;
-  DECLARE current_user_id INT;
-  
-  DECLARE post_id_cursor CURSOR FOR 
-    SELECT post_id 
-      FROM notifications;
-
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-  OPEN post_id_cursor;
-    post_loop: LOOP
-      FETCH post_id_cursor INTO current_post_id;
-        IF done THEN
-            LEAVE post_loop;
-        END IF;
-
-      SELECT user_id INTO current_user_id
-      FROM posts
-      WHERE post_id = current_post_id;
-
-      INSERT INTO fetched_users 
-        (post_id, user_id) 
-      VALUES 
-        (current_post_id, current_user_id);
-    END LOOP;
-
-    CLOSE post_id_cursor;
-END ;;
 
 CREATE OR REPLACE VIEW notification_posts AS
 SELECT 
@@ -102,15 +65,11 @@ SELECT
     p.post_id,
     p.content
 FROM notifications n
-INNER JOIN fetched_users fu 
-  ON fu.post_id = n.post_id
-INNER JOIN users u 
-  ON u.user_id = fu.user_id
 INNER JOIN posts p 
   ON p.post_id = n.post_id
+INNER JOIN users u 
+  ON u.user_id = p.user_id
 ORDER BY p.post_id;
-
-CALL fetch_user_ids();
 
 CREATE TRIGGER after_user_insert 
 AFTER INSERT ON users
@@ -167,6 +126,12 @@ BEGIN
       WHERE user_id = user_id;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    INSERT INTO posts
+      (iser_id, content)
+    VALUES
+      (p_user_id, p_content)
+    SET last_post_id = LAST_INSERT_ID();
 
     OPEN friends_cursor;
 
